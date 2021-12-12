4cd9f9e690cef8795c167ce9a06724c96f32a105
lenrek88 <lenrek88@yandex.ru>
2021-12-12 18:16:34 +0300

    Update to country

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 const UI = {
     INPUT: document.querySelector('input[type=text]'),
     BUTTON:  document.querySelector('input[type=button]'),
-    TEXT:  document.querySelector('p')
+    TEXT:  document.querySelectorAll('p')
 }
 
-const SERVER_URL = 'https://api.genderize.io';
+const URL = {
+    GENDER: 'https://api.genderize.io',
+    NATIONAL: 'https://api.nationalize.io'
+}
 
 function alertGender(){
     const firstName = UI.INPUT.value;
-    const url = `${SERVER_URL}?name=${firstName}`;
+    const url = `${URL.GENDER}?name=${firstName}`;
     fetch(url)
         .then(response => response.json())
         .then(units => {
             if (units.gender === 'male' || units.gender === 'female') {
-                UI.TEXT.textContent = `${UI.INPUT.value} is ${units.gender}`;
+                UI.TEXT[0].textContent = `${UI.INPUT.value} is ${units.gender}`;
             } else
                  {
-                     UI.TEXT.textContent = `Error :(`
+                     UI.TEXT[0].textContent = `Error :(`
                  }
         });
+    alertCountry(firstName);
+}
+
+function alertCountry(firstName) {
+    const url = `${URL.NATIONAL}?name=${firstName}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(units => {
+            UI.TEXT[1].textContent = `Your country ${units.country[0].country_id} or ${units.country[1].country_id}`
+        })
 }
 
 
